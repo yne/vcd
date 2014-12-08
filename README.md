c636acbc7ab1f3200104239481ff8242e7fa6432
yne <yne@users.noreply.github.com>
2014-12-08 22:58:52 +0100

    better parsing. scope filtering

--- README.md
+++ README.md
@@ -1,4 +1,95 @@
-vcd
-===
+# vcd
 
 VCD file command line viewer
+
+## Usage
+
+vcd [FILE] [OPTION(S)]
+
+### FILE :
+A valid .vcd file (can be passed throught the `-i` options too)
+
+### OPTIONS :
+* -h	: display the help screen
+* -v=0	: verbose level (0:fatal,1:error,2:warning,3:debug)
+* -w=2	: sample ascii width
+* -r=2	: rounded wave (0:none,1:pipe,2:slash)
+* -i=file	: input file
+* -s=a,b,c	: scope(s) to display
+
+### Note
+`stdout` will be used as input stream  if no input file were providen.
+
+So you could do :
+
+    vcd  < in.vcd > out.txt
+
+## Examples
+
+    vcd sim_fifo.vcd
+
+will output
+
+```
+43 samples / Fri Nov 21 16:56:29 2014 / 1 fs
+       e_clk(!)[ 1]: ¯¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_
+ 
+       e_rst(")[ 1]: ______/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+ 
+       e_ren(#)[ 1]: ¯¯¯¯¯¯¯¯¯¯\_______/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\___/¯¯¯\___________________/¯¯¯¯¯
+ 
+    e_d[3:0](%)[ 4]:  Z Z Z Z Z 1 1 Z Z Z Z 2 2 3 3 4 4 5 5 Z Z 6 6 Z Z 7 7 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z
+ 
+    e_q[3:0](&)[ 4]:  U U Z Z Z Z 0 0 1 1 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z 3 3 Z Z 4 4 5 5 6 6 7 7 0 0 Z Z
+ 
+     e_empty(')[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+ 
+       e_mid(()[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+ 
+      e_full())[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+ 
++-- fifo1
+|        clk(*)[ 1]: ¯¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_
+|
+|        rst(+)[ 1]: ______/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+|
+|        ren(,)[ 1]: ¯¯¯¯¯¯¯¯¯¯\_______/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\___/¯¯¯\___________________/¯¯¯¯¯
+|
+|        wen(-)[ 1]: ¯¯¯¯¯¯¯¯¯¯\___/¯¯¯¯¯¯¯\_______________/¯¯¯\___/¯¯¯\___/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+|
+|     d[3:0](.)[ 4]:  Z Z Z Z Z 1 1 Z Z Z Z 2 2 3 3 4 4 5 5 Z Z 6 6 Z Z 7 7 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z
+|
+|     q[3:0](/)[ 4]:  U U Z Z Z Z 0 0 1 1 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z 3 3 Z Z 4 4 5 5 6 6 7 7 0 0 Z Z
+|
+|      empty(0)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+|
+|        mid(1)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+|
+|       full(2)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+|
+| w_adr[1:0](3)[ 2]:  U U 0 0 0 0 1 1 1 1 1 1 2 2 3 3 0 0 1 1 1 1 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+|
+| r_adr[1:0](4)[ 2]:  U U 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 3 3 3 3 0 0 1 1 2 2 3 3 3 3 3 3
+|
+       e_wen(^)[ 1]: ____________________________________________________________________________________
+```
+
+    vcd -i=sim_fifo.vcd -w=1 -r=0 -s=fifo1
+
+will output only the fifo1 module in a smaller graph without transitions
+
+```
+43 samples / Fri Nov 21 16:56:29 2014 / 1 fs
++-- fifo1
+|        clk(*)[ 1]: ¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯  
+|        rst(+)[ 1]: ___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  
+|        ren(,)[ 1]: ¯¯¯¯¯____¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯__¯¯__________¯¯¯¯  
+|        wen(-)[ 1]: ¯¯¯¯¯__¯¯¯¯________¯¯__¯¯__¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  
+|     d[3:0](.)[ 4]: ZZZZZ11ZZZZ22334455ZZ66ZZ77ZZZZZZZZZZZZZZZZ  
+|     q[3:0](/)[ 4]: UUZZZZ0011ZZZZZZZZZZZZZZZZ33ZZ4455667700ZZZ  
+|      empty(0)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
+|        mid(1)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
+|       full(2)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
+| w_adr[1:0](3)[ 2]: UU00001111112233001111222233333333333333333  
+| r_adr[1:0](4)[ 2]: UU00000011111111111111222233330011223333333
+```
--- vcd.c
+++ vcd.c
@@ -5,180 +5,213 @@
 
 #define MAX_SAMPLE 256
 #define MAX_CHANNEL 128
-#define MAX_NAME 16//change the scanf too
-#define COUNT(A) (sizeof(A)/sizeof(A[0]))
+#define MAX_NAME 32//change the scanf too
+#define COUNT(A) (sizeof(A)/sizeof((A)[0]))
 
 typedef struct{
 	int width;
 	int verbose;
 	int round;
+	char*scope;
+	FILE*fin;
+	FILE*fout;
 }Parameters;
 
 typedef struct{
 	char size;
+	char scope;
 	char name[MAX_NAME];
-	char type[MAX_SAMPLE];//U,Z,Data...
-	unsigned values[MAX_SAMPLE];
+	char    type[MAX_SAMPLE];//'U','Z','\0'=Data
+	unsigned val[MAX_SAMPLE];
 }Channel;
 
-int nb_timestamps=0;
-long long unsigned timestamps[MAX_SAMPLE];
-Channel channels[MAX_CHANNEL];
+typedef struct{
+	long long unsigned timestamps[MAX_SAMPLE];
+	Channel ch[MAX_CHANNEL];
+	char scopes[32][32];//[0]=root
+	int nb,nb_scopes;
+	char date[32],version[32],scale[32];//file info
+	//parsing related values
+	int cur_scopes;
+}Parser;
 
-void parseInst(FILE*f){
-	char data[256];
-	int c,data_pos=0;
-
-	while((c=fgetc(f))!=EOF){
-		if(c=='$'){//maybe it's a $end ?
-			int pos=ftell(f);//save the pos just after the $ (in case it's not a $end)
-			if((fgetc(f)=='e')&&(fgetc(f)=='n')&&(fgetc(f)=='d')&&(!isalpha(fgetc(f)))){
-				data[data_pos]='\0';
-				
-				//printf("instr:%s\n",data);
-				if(strstr(data,"var reg")){//new register !
-					int size;char name[MAX_NAME],id;
-					if(sscanf(data,"var reg %i %c %15s",&size,&id,&name)==3){
-						channels[id].size=size;
-						memcpy(channels[id].name,name,sizeof(name)-1);
-						//printf(">>>>size:%i id:%c name:%s\n",size,id,name);
-					}
-				}
-				return;
-			}else{
-				fseek(f,pos,SEEK_SET);
-			}
-		}
-		if(data_pos<sizeof(data)-1)
-			data[data_pos++]=c;
-	}
-}
-
-void parseTime(FILE*f){
-	long long unsigned stamp=0;
-	int i;char c;
-	while((c=fgetc(f))!=EOF){
-		if(!isdigit(c)){
-			ungetc(c,f);
-			if(nb_timestamps<COUNT(timestamps)){
-				if(nb_timestamps>0){//copy all previous channels_size values to the current one
-					for(i=0;i<COUNT(channels);i++){
-						channels[i].values[nb_timestamps]=channels[i].values[nb_timestamps-1];
-						channels[i].type  [nb_timestamps]=channels[i].type  [nb_timestamps-1];
-					}
-				}
-				timestamps[nb_timestamps]=stamp;
-				nb_timestamps++;
-			}
-			//printf("time:%llu\n",stamp);
-			return;
-		}
-		stamp=stamp*10+(c-'0');
-	}
-}
-
-void parseData(FILE*f){
-	unsigned data=0,base=10;
-	char type,c,id='?';
-	while((c=fgetc(f))!=EOF){
-		if(isdigit(c))
-			data=data*base+(c-'0');
-		else if(c=='b')base=2;
-		else if(c=='x')base=16;
-		else if(c=='Z')type='Z';//undefined type
-		else if(c=='U')type='U';//undefined type
-		else if(c==' ');
-		else{//unexpected character = end ?
-			id=c;
-			if(!nb_timestamps)
-				return printf("timestamps unpreceded data\n");
-			if(!channels[id].size)
-				return ;//printf("undeclared channel (%c)\n",id);
-			channels[id].values[nb_timestamps-1]=data;
-			return;
-		}
-	}
-}
-
-void parseFile(FILE*f){
-	int c;
-	while((c=fgetc(f))!=EOF){
-		if(isspace(c))continue;
-		if(isdigit(c) || c=='b' || c=='Z' || c=='U'){
-			ungetc(c,f);
-			parseData(f);
-		}else if(c=='$'){
-			parseInst(f);
-		}else if(c=='#'){
-			parseTime(f);
-		}else{
-			printf("unknow char : %c\n",c);
-		}
-	}
-}
-void showVertical(Parameters*params){
-	int chan,smpl,w;
-	for(chan=0;chan<COUNT(channels);chan++){
-		if(!channels[chan].size)continue;//skip empty channels
-		if(params->width>=2)printf("\n");
-		printf("% 10s(%c)[%2i]: ",channels[chan].name,chan,channels[chan].size);
-		for(smpl=0;smpl<COUNT(timestamps) && timestamps[smpl]!=~0 ;smpl++){
-			char type=channels[chan].type[smpl];
-			unsigned data=channels[chan].values[smpl];
-			if(channels[chan].size==1){//binary
-				w=params->width;
-				if(params->round && smpl>0 && !channels[chan].type[smpl-1]){//have a previous data => can print a transition
-					if(channels[chan].values[smpl]!=channels[chan].values[smpl-1]){//the value changed
-						printf(params->round==2?"%c":"|",(channels[chan].values[smpl-1]?'\\':'/'));//from H to L or L to H ?
-						w-=1;
-					}
-				}
-				while(w-->0)printf("%c",type?type:(data?238:95));
-			}else{//bus
-				if(channels[chan].type[smpl])//not a data
-					printf("%c",params->width,channels[chan].type[smpl]);
-				else
-					printf("%*X",params->width,channels[chan].values[smpl]);
-			}
-		}
-		printf("\n");
-	}
-}
-void showHelp(char*arg0,Parameters*p){
-	printf("Usage: %s [FILE] [OPTION]...:\n"
+int showHelp(char*arg0,Parameters*p){
+	return fprintf(stderr,"Usage: %s [FILE] [OPTION]...:\n"
 				" -h	: display this help screen\n"
 				" -v=%i	: verbose level (0:fatal,1:error,2:warning,3:debug)\n"
 				" -w=%i	: sample ascii width\n"
 				" -r=%i	: rounded wave (0:none,1:pipe,2:slash)\n"
+				" -i=file	: input file\n"
+				" -s=a,b,c	: scope(s) to display\n"
 				,arg0,p->verbose,p->width,p->round);
 }
-int main(int argc,char**argv){
-	//init context
-	memset(channels,0,sizeof(channels));
-	memset(timestamps,~0,sizeof(timestamps));
+
+int parseArgs(int argc,char**argv,Parameters*params){
+	int i;
+	for(i=1;i<argc;i++){//parse "-" arguments
+		if(argv[i][0]!='-'){
+			params->fin=fopen(argv[i],"r");
+		}else{
+			switch(argv[i][1]){
+				case 'i':params->fin=fopen(argv[i]+3,"r");break;
+				case 'o':params->fout=fopen(argv[i]+3,"w+");break;
+				case 'h':showHelp(argv[0],params);exit(0);break;
+				case 'w':params->width=atoi(argv[i]+3);break;
+				case 'r':params->round=atoi(argv[i]+3);break;
+				case 's':params->scope=argv[i]+3;break;
+				default:fprintf(stderr,"unknow param '%c'",argv[i][1]);
+			}
+		}
+	}
+}
+
+void parseInst(Parameters*params,Parser*p){
+	char token[32];
+	char data[128];
+	int c,data_pos=0;
 	
+	fscanf(params->fin,"%31s",token);
+	//printf("%s\n",token);
+	if(!strcmp("var",token)){
+		char id='\0',size=0;Channel tmp={};
+		fscanf(params->fin," reg %d %c %31[^ $]",&(tmp.size),&id,&(tmp.name));//space in name allowed ?
+		p->ch[id]=tmp;//printf("size=%i <%c> name=<%s>\n",size,id,data);
+		p->ch[id].scope=p->cur_scopes;
+	}
+	else if(!strcmp("scope",token))         {fscanf(params->fin,"%*127s %127[^ $]",p->scopes[p->cur_scopes=++(p->nb_scopes)]);}
+	else if(!strcmp("date",token))          {fscanf(params->fin,"\n%31[^$\n]",p->date);}
+	else if(!strcmp("version",token))       {fscanf(params->fin,"\n%31[^$\n]",p->version);}
+	else if(!strcmp("timescale",token))     {fscanf(params->fin,"\n%127[^$\n]",p->scale);}
+	else if(!strcmp("comment",token))       {fscanf(params->fin,"\n%*[^$]");}
+	else if(!strcmp("upscope",token))       {fscanf(params->fin,"\n%*[^$]");p->cur_scopes=0;}/*back to root */
+	else if(!strcmp("enddefinitions",token)){fscanf(params->fin,"\n%*[^$]");}
+	else if(!strcmp("end",token)){}
+	else {printf("unknow token : %s\n",token);}
+}
+
+void parseTime(Parameters*params,Parser*p){
+	long long unsigned stamp=0;
+	int i;char c;
+	while((c=fgetc(params->fin))!=EOF){
+		if(isdigit(c)){
+			stamp=stamp*10+(c-'0');
+		}else{
+			ungetc(c,params->fin);
+			if(p->nb >= COUNT(p->timestamps))return;
+			if(p->nb > 0){//copy all previous channels_size val to the current one
+				for(i=0;i<COUNT(p->ch);i++){
+					p->ch[i].val [p->nb]=p->ch[i].val [p->nb-1];
+					p->ch[i].type[p->nb]=p->ch[i].type[p->nb-1];
+				}
+			}
+			p->timestamps[p->nb]=stamp;
+			p->nb++;
+			return;
+		}
+	}
+}
+
+void parseData(Parameters*params,Parser*p){
+	unsigned data=0,base=10;
+	char type='\0',id='\0';
+	int c=fgetc(params->fin);
+	if(c=='b'){//parsing bus data b%[0-9UZ]+ %c
+		base=2;
+		while((c=fgetc(params->fin))!=EOF && c!=' '){
+			if(isdigit(c))data=data*base+(c-'0');
+			else type=c;//letter (Z,U,...) = undefined type, but we don't know the id yet
+		}
+		id=fgetc(params->fin);
+		p->ch[id].type[p->nb-1]=type;
+		p->ch[id].val [p->nb-1]=data;
+	}else{//parsing state %[0-9UZ] %c
+		id=fgetc(params->fin);
+		if(isalpha(c))p->ch[id].type[p->nb-1]=c;
+		if(isdigit(c))p->ch[id].val [p->nb-1]=c-'0';
+	}
+}
+
+void parseFile(Parameters*params,Parser*p){
+	int c;
+	while((c=fgetc(params->fin))!=EOF){
+//		printf("?%c\n",c);
+		if(isspace(c))continue;
+		if(c=='$'){
+			parseInst(params,p);
+		}else if(isdigit(c) || c=='b' || c=='Z' || c=='U'){
+			ungetc(c,params->fin);
+			parseData(params,p);
+		}else if(c=='#'){
+			parseTime(params,p);
+		}else{
+			fprintf(stderr,"unknow char : %c\n",c);
+		}
+	}
+}
+
+void showVertical(Parameters*params,Parser*p){
+	int chan,smpl,w,prev_scope=0;
+	
+	if(p->nb      )fprintf(params->fout,"%i samples",p->nb);
+	if(p->date [0])fprintf(params->fout," / %s",p->date);
+	if(p->scale[0])fprintf(params->fout," / %s",p->scale);
+	fprintf(params->fout,"\n");
+	
+	for(chan=0;chan < COUNT(p->ch);chan++){
+		if(!p->ch[chan].size)continue;//skip empty ch
+		if(params->scope && (!p->ch[chan].scope || !strstr(params->scope,p->scopes[p->ch[chan].scope])))
+			continue;//skip root node or unrelated node if scope-only wanted
+		
+		
+		if((!chan && p->ch[chan].scope) || (chan>0 && (p->ch[chan].scope!=p->ch[chan-1].scope)))
+			fprintf(params->fout,"+-- %s\n",p->ch[chan].scope?p->scopes[p->ch[chan].scope]:"");
+		
+		fprintf(params->fout,"%c % 10s(%c)[%2i]: ",p->ch[chan].scope?'|':' ',p->ch[chan].name,chan,p->ch[chan].size);
+		for(smpl=0;smpl < p->nb ;smpl++){
+			char     type = p->ch[chan].type[smpl];
+			unsigned data = p->ch[chan].val [smpl];
+			if(p->ch[chan].size==1){//binary
+				w=params->width;
+				//have a previous data => can print a transition
+				if(params->round && smpl>0 && !p->ch[chan].type[smpl-1]){
+					if(p->ch[chan].val[smpl]!=p->ch[chan].val[smpl-1]){//the value changed
+						//from H to L or L to H ?
+						fprintf(params->fout,params->round==2?"%c":"|",(p->ch[chan].val[smpl-1]?'\\':'/'));
+						w-=1;
+					}
+				}
+				while(w-->0)fprintf(params->fout,"%c",type?type:(data?238:95));
+			}else{//bus
+				if(p->ch[chan].type[smpl])//not a data
+					fprintf(params->fout,"%*c",params->width,p->ch[chan].type[smpl]);
+				else
+					fprintf(params->fout,"%*X",params->width,p->ch[chan].val[smpl]);
+			}
+		}
+		fprintf(params->fout,"%c%c\n",params->width>=2?'\n':' ',p->ch[chan].scope && params->width>=2?'|':' ');
+	}
+}
+
+int main(int argc,char**argv){
 	Parameters params={
 		.verbose=0,
 		.width=2,
 		.round=2,
+		.fin=stdin,
+		.fout=stdout,
 	};
-	// load arguments
-	FILE*f=((argc>1)&&(argv[1][0]!='-'))?fopen(argv[1],"r"):stdin;
-	if(!f)return printf("unable to open %s\n",argv[1]);
-	int i;
-	for(i=1;i<argc;i++){//parse "-" arguments
-		if(argv[i][0]!='-')continue;
-		switch(argv[i][1]){
-			case 'h':showHelp(argv[0],&params);return 0;break;
-			case 'w':params.width=atoi(argv[i]+3);break;
-			case 'r':params.round=atoi(argv[i]+3);break;
-			default:printf("skiped param '%c'",argv[i][1]);
-		}
-	}
+	Parser data;
+	memset(&data,0,sizeof(Parser));
 
-	parseFile(f);
-	showVertical(&params);
+	parseArgs(argc,argv,&params);
+	if(params.fin){
+		parseFile(&params,&data);
+		showVertical(&params,&data);
+	}else{
+		fprintf(stderr,"no input stream\n");
+	}
 	
-	fclose(f);
+	fclose(params.fin);
+	
 	return 0;
 }

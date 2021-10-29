058e7563f3890d94ba601358c74c2e964c51c80c
Алексей Муратов <50487552+MuratovAS@users.noreply.github.com>
2021-10-30 03:39:07 +0500

    Added support for icarus vvp through UZX command support  (#4)

--- vcd.c
+++ vcd.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_SAMPLE 512
+#define MAX_SAMPLE 2048
 #define MAX_CHANNEL 400
 #define MAX_NAME 128
 #define COUNT(A) (sizeof(A)/sizeof((A)[0]))
@@ -25,7 +25,7 @@ typedef struct{
 	int      size;
 	int      scope;
 	char     name[MAX_NAME];
-	char     type[MAX_SAMPLE];//'U','Z','\0'=Data
+	char     type[MAX_SAMPLE];//'U','Z','X','\0'=Data
 	unsigned val [MAX_SAMPLE];
 }Channel;
 
@@ -41,12 +41,12 @@ typedef struct{
 
 void showHelp(char*arg0,Parameters*p){
 	fprintf(stderr,"Usage: %s [OPTION] [FILE]...:\n"
-	       " -h       : this help screen\n"
-	       " -v=%i    : verbosity (0:fatal,1:error,2:warning,3:debug)\n"
-	       " -w=%i    : width of each sample  (1,2,...)\n"
-	       " -r=%i    : rounded wave (0:none,1:pipe,2:slash)\n"
-	       " -c=%i    : column width\n"
-	       " -s=a,b,c : comma separated scope(s) to display\n"
+	       " -h:			:this help screen\n"
+	       " -v=%i			: verbosity (0:fatal,1:error,2:warning,3:debug)\n"
+	       " -w=%i			: width of each sample  (1,2,...)\n"
+	       " -r=%i			: rounded wave (0:none,1:pipe,2:slash)\n"
+	       " -c=%i			: column width\n"
+	       " -s=a,b,c		: comma separated scope(s) to display\n"
 	       ,arg0,p->verbose,p->width,p->round,p->colsize);
 }
 
@@ -85,7 +85,8 @@ void parseInst(Parameters*params,Parser*p){
 	if(!strcmp("var",token)){
 		char id_str[4];
 		Channel chan={};
-		fscanf(params->fin," reg %d %3[^ ] %"TXT(MAX_NAME)"[^ $]",&(chan.size),id_str,chan.name);
+		fscanf(params->fin," reg %d %3[^ ] %"TXT(MAX_NAME)"[^$]",&(chan.size),id_str,chan.name);
+		fscanf(params->fin," wire %d %3[^ ] %"TXT(MAX_NAME)"[^$]",&(chan.size),id_str,chan.name);
 		int id=char2id(id_str);
 		p->ch[id]=chan;//printf("size=%i <%c> name=<%s>\n",size,id,data);
 		p->ch[id].scope=p->cur_scopes;
@@ -97,6 +98,7 @@ void parseInst(Parameters*params,Parser*p){
 	else if(!strcmp("comment",token))       {fscanf(params->fin,"\n%*[^$]");}
 	else if(!strcmp("upscope",token))       {fscanf(params->fin,"\n%*[^$]");p->cur_scopes=0;}/*back to root */
 	else if(!strcmp("enddefinitions",token)){fscanf(params->fin,"\n%*[^$]");}
+	else if(!strcmp("dumpvars",token))      {}
 	else if(!strcmp("end",token))           {}
 	else {printf("unknow token : %s\n",token);}
 }
@@ -131,19 +133,24 @@ void parseData(Parameters*params,Parser*p){
 	unsigned data=0;
 	char id_str[4],type='\0';
 	int id=0,c=fgetc(params->fin);
-	if(c=='b'){//parsing bus data b%[0-9UZ]+ %c
+	if(c=='b'){//parsing bus data b%[0-9UZX]+ %c
 		while((c=fgetc(params->fin))!=EOF && c!=' '){
 			if(isdigit(c))data=data*2+(c-'0');
-			else type=c;//letter (Z,U,...) = undefined type, but we don't know the id yet
+			else{
+				//letter (Z,U,X,...) = undefined type, but we don't know the id yet
+				if(c=='x' || c=='z') type=toupper(c);
+				else type=c;
+			}
 		}
 		fscanf(params->fin,"%3[^\n]",id_str);
 		id=char2id(id_str);
 		p->ch[id].type[p->nb-1]=type;
 		p->ch[id].val [p->nb-1]=data;
-	}else{//parsing state %[0-9UZ] %c
+	}else{//parsing state %[0-9UZX] %c
 		fscanf(params->fin,"%3[^\n]",id_str);
 		id=char2id(id_str);
 		if(isalpha(c))p->ch[id].type[p->nb-1]=c;
+		else p->ch[id].type[p->nb-1]='\0';//letter (Z,U,X,...) = undefined type
 		if(isdigit(c))p->ch[id].val [p->nb-1]=c-'0';
 	}
 }
@@ -155,8 +162,9 @@ void parseFile(Parameters*params,Parser*p){
 		if(isspace(c))continue;
 		if(c=='$'){
 			parseInst(params,p);
-		}else if(isdigit(c) || c=='b' || c=='Z' || c=='U'){
-			ungetc(c,params->fin);
+		}else if(isdigit(c) || c=='b' || c=='Z' || c=='U' || c=='x' || c=='z'){
+			if(c=='x' || c=='z')ungetc(toupper(c),params->fin);
+			else ungetc(c,params->fin);
 			parseData(params,p);
 		}else if(c=='#'){
 			parseTime(params,p);
@@ -167,22 +175,22 @@ void parseFile(Parameters*params,Parser*p){
 }
 
 void showVertical(Parameters*params,Parser*p){
-	int chan,smpl,w;
-	
+	int w;
+	unsigned chan,smpl;
 	if(p->nb      )fprintf(params->fout,"%i samples",p->nb);
 	if(p->date [0])fprintf(params->fout," / %s",p->date);
 	if(p->scale[0])fprintf(params->fout," / %s",p->scale);
 	fprintf(params->fout,"\n");
-	
+
 	for(chan=0;chan < COUNT(p->ch);chan++){
 		if(!p->ch[chan].size)continue;//skip empty ch
 		if(params->scope && (!p->ch[chan].scope || !strstr(params->scope,p->scopes[p->ch[chan].scope])))
 			continue;//skip root node or unrelated node if scope-only wanted
 		if((!chan && p->ch[chan].scope) || (chan>0 && (p->ch[chan].scope!=p->ch[chan-1].scope)))
 			fprintf(params->fout,"┌── %s\n",p->ch[chan].scope?p->scopes[p->ch[chan].scope]:"");
-		
+
 		fprintf(params->fout,"%s %*.*s[%2i]: ",p->ch[chan].scope?"│":" ",params->colsize,params->colsize,p->ch[chan].name,p->ch[chan].size);
-		for(smpl=0;smpl < p->nb ;smpl++){
+		for(smpl=0;smpl < p->nb ;smpl+=1){
 			char     type = p->ch[chan].type[smpl];
 			unsigned data = p->ch[chan].val [smpl];
 			if(p->ch[chan].size==1){//binary
@@ -197,7 +205,7 @@ void showVertical(Parameters*params,Parser*p){
 				}
 				while(w-->0){
 					if(type)fprintf(params->fout,"%c",type);
-					else    fprintf(params->fout,"%s",data?"▔":"_");
+					else    fprintf(params->fout,"%s",data?"▔":"▁");
 				}
 			}else{//bus
 				int is_bin = p->ch[chan].type[smpl];

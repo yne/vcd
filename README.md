16c107790d23f3747134f630f08555df80538194
Rémy F <yne@users.noreply.github.com>
2021-10-31 11:10:24 +0100

    Use a pipe-friendly output

--- README.md
+++ README.md
@@ -1,49 +1,93 @@
-<img src=https://svgur.com/i/PAx.svg width=100% height=200>
+<img src=https://svgur.com/i/bp1.svg width=100% height=200>
 
-VCD file command line viewer
+Value Change Dump (.vcd) command line viewer for Windows, Linux and MacOS
 
-Lightweight command line remplacement to GTKWave (16801 KB vs 6 KB)
+```yml
+# vcd < sample/basic.vcd
+global:
+  zoom: 2
+  date: Fri Nov 21 16:56:29 2014
+  total: 43
+  time:
+    scale: 1.00
+    unit: fs
+    line        : "0                   10                  20                  30"
+channels:
+  default:
+    e_clk       : "▔▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔"
+    e_rst       : "▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+    e_ren       : "▔▔▔▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔╲▁▁▁"
+    e_wen       : "▔▔▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔╲▁▁▁╱▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔"
+    e_d[3:0]    : "Z Z Z Z Z 1 1 Z Z Z Z 2 2 3 3 4 4 5 5 Z Z 6 6 Z Z 7 7 Z Z Z Z "
+    e_q[3:0]    : "U U Z Z Z Z 0 0 1 1 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z 3 3 Z Z 4 "
+    e_empty     : "U U ▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+    e_mid       : "U U ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+    e_full      : "U U ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔╲▁"
+  fifo1:
+    clk         : "▔▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔╲▁╱▔"
+    rst         : "▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+    ren         : "▔▔▔▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔╲▁▁▁"
+    wen         : "▔▔▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔╲▁▁▁╱▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔"
+    d[3:0]      : "Z Z Z Z Z 1 1 Z Z Z Z 2 2 3 3 4 4 5 5 Z Z 6 6 Z Z 7 7 Z Z Z Z "
+    q[3:0]      : "U U Z Z Z Z 0 0 1 1 Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z Z 3 3 Z Z 4 "
+    empty       : "U U ▔▔▔▔▔▔▔▔╲▁▁▁╱▔▔▔▔▔▔▔╲▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+    mid         : "U U ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+    full        : "U U ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁╱▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔╲▁"
+    w_adr [1:0] : "U U 0 0 0 0 1 1 1 1 1 1 2 2 3 3 0 0 1 1 1 1 2 2 2 2 3 3 3 3 3 "
+    r_adr [1:0] : "U U 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 3 3 3 3 0 "
+```
 
 ## Download
 
-Windows, Linux and MacOS binaries are available in the [releases page](../../releases)
+See [releases page](../../releases)
+
+## Build
+
+```bash
+cc vcd.c -o vcd
+```
 
 ## Usage
 
-```
-Usage: vcd [OPTION]... [FILE]
-
- -h     : display this help screen
- -v=0   : verbose level (0:fatal,1:error,2:warning,3:debug)
- -w=2   : sample ascii width (1,2,...)
- -r=2   : rounded wave (0:none,1:pipe,2:slash)
- -c=32  : name column width
- -t=0   : time scale step (0:none,1,10,...)
- -s=a,b : comma separated scopes to display
+```bash
+vcd < in.vcd            # output to screen
+vcd < in.vcd > out.yml  # output to file
+vcd < in.vcd | less -S  # scrollable view
 ```
 
-## Examples
+## Query
+
+The default `vcd` output is a valid YAML file, so you can process it with many tools like yq (a `jq` for yaml)
 
 ```bash
-vcd sim_fifo.vcd
-vcd  < in.vcd > out.txt
-vcd -i=sim_fifo.vcd -w=1 -r=0 -s=fifo1
+# print the first 9 sample of the clk channel in the fifo1 scope
+vcd < samples/basic.vcd | yq .channels.fifo1.clk[0:9]
 ```
 
-# Preview
+## Styling
 
+Waveform, position, color can be customized using environment variables.
+
+```bash
+# skip the 9 first samples
+SKIP=9 vcd < in.vcd | less -S
+# Disable RAISE/DROWN transition
+RAISE="" DROWN="" vcd < in.vcd
+# color the bus value in green using StartTeX/EndTeXt (STX/ETX) variables
+STX=$(printf "\x1b[32m") ETX=$(printf "\x1b[0m") vcd < in.vcd
 ```
-43 samples / Fri Nov 21 16:56:29 2014 / 1 fs
-+-- fifo1
-|        clk(*)[ 1]: ¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯_¯  
-|        rst(+)[ 1]: ___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  
-|        ren(,)[ 1]: ¯¯¯¯¯____¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯__¯¯__________¯¯¯¯  
-|        wen(-)[ 1]: ¯¯¯¯¯__¯¯¯¯________¯¯__¯¯__¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  
-|     d[3:0](.)[ 4]: ZZZZZ11ZZZZ22334455ZZ66ZZ77ZZZZZZZZZZZZZZZZ  
-|     q[3:0](/)[ 4]: UUZZZZ0011ZZZZZZZZZZZZZZZZ33ZZ4455667700ZZZ  
-|      empty(0)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
-|        mid(1)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
-|       full(2)[ 1]: UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU  
-| w_adr[1:0](3)[ 2]: UU00001111112233001111222233333333333333333  
-| r_adr[1:0](4)[ 2]: UU00000011111111111111222233330011223333333
+
+## Auto-Refresh
+
+The following snippet endlessly wait for a change on the `in.vcd` file.
+Then, when it happen, regenerate the `out.yml` graph and refresh the `less -S out.yml` scrollable display by sending a "H" keypress.
+
+```bash
+echo "hi ! please modify 'in.vcd' to refresh me" > out.yml
+while inotifywait -q -e modify -e close_write in.vcd ; do
+  vcd < in.vcd > out.yml           # regenerate out.yml
+  screen -x -r bridge -X stuff R   # send Refresh command to foreground less -S
+done &                             # this loop will run in background
+screen -S bridge less -S out.yml   # the foreground "less -S" display
+# don't forget to stop the background loop using `kill %1` or fg + ^C
 ```
--- vcd.c
+++ vcd.c
@@ -3,330 +3,219 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_SAMPLE 2048
-#define MAX_CHANNEL 400
-#define MAX_NAME 128
-#define COUNT(A) (sizeof(A) / sizeof((A)[0]))
+#define USAGE "USAGE: vcd < in.vcd > out.ascii :\n"
+#define PROLOG "Fatal error. Send your VCD at https://github.com/yne/vcd/issue"
+#define die(...) exit(fprintf(stderr, PROLOG "\nReason: " __VA_ARGS__))
+
+#define SFL 127             // scanf limit (for MAX_NAME)
+#define MAX_NAME (SFL + 1)  // parsing token (channel name, scope name, ...)
+#define MAX_SCOPE 32        // how many char scopes[] to allocate
+#define MAX_CHANNEL 400     // how many Channel to allocate 96*96 = 8836
+#define MAX_SAMPLE 2048     // how many Sample to allocate
+#define ITV_TIME 10         // sample interval to display timestamp
+#define VALUES "0123456789zZxXbU-"  // allowed bus values/types
+#define COUNT(A) (sizeof(A) / sizeof(*A))
 #define MAX(A, B) (A > B ? A : B)
 #define VAL(A) #A
 #define TXT(A) VAL(A)
 
 typedef struct {
-  int width;
-  int verbose;
-  int round;
-  int colsize;
-  int timescalestep;
-  char* scope;
-  FILE* fin;
-  FILE* fout;
-} Parameters;
-
+  char *low, *raise, *high, *drown, *start, *end;
+  int skip;
+} PrintOpt;
+typedef struct {
+  // could have as much state as it bus size but nobody handle such case
+  // 'UZX' => show state
+  // '\0' => show .val
+  char state;
+  unsigned val;
+} Sample;
 typedef struct {
   int size;
   int scope;
   char name[MAX_NAME];
-  char type[MAX_SAMPLE];  //'U','Z','X','\0'=Data
-  unsigned val[MAX_SAMPLE];
+  Sample samples[MAX_SAMPLE];
 } Channel;
 
 typedef struct {
   long long unsigned timestamps[MAX_SAMPLE];
-  Channel ch[MAX_CHANNEL];
-  char scopes[32][32];  //[0]=root
-  int nb, nb_scopes;
-  char date[32], version[32], scale[32];  // file info
+  Channel ch[MAX_CHANNEL];           // [0] = timestamps
+  char scopes[MAX_SCOPE][MAX_NAME];  //[0] = default
+  int total, scope_count;
+  double scale;  // duration of each sample
+  char date[MAX_NAME], version[MAX_NAME], unit[MAX_NAME];  // file info
   // parsing related values
-  int cur_scopes;
-} Parser;
+  int scope_cur;
+  unsigned scope_lim, ch_lim, sz_lim;
+} ParseCtx;
 
-void showHelp(char* arg0, Parameters* p) {
-  fprintf(
-      stderr,
-      "Usage: %s [OPTION] [FILE]...:\n"
-      " -h:			: this help screen\n"
-      " -v=%i			: verbosity "
-      "(0:fatal,1:error,2:warning,3:debug)\n"
-      " -w=%i			: width of each sample  (1,2,...)\n"
-      " -r=%i			: rounded wave (0:none,1:pipe,2:slash)\n"
-      " -c=%i			: column width\n"
-      " -t=%i			: time scale step (0:none,1,10,...)\n"
-      " -s=a,b,c		: comma separated scope(s) to display\n",
-      arg0, p->verbose, p->width, p->round, p->colsize, p->timescalestep);
-}
-
-void parseArgs(int argc, char** argv, Parameters* params) {
-  int i;
-  for (i = 1; i < argc; i++) {  // parse "-" arguments
-    if (argv[i][0] != '-') {
-      params->fin = fopen(argv[i], "r");
-      continue;
-    }
-    switch (argv[i][1]) {
-      case 'o':
-        params->fout = fopen(argv[i] + 3, "w+");
-        break;
-      case 'h':
-        showHelp(argv[0], params);
-        exit(0);
-        break;
-      case 'w':
-        params->width = MAX(1, atoi(argv[i] + 3));
-        break;
-      case 'r':
-        params->round = atoi(argv[i] + 3);
-        break;
-      case 'c':
-        params->colsize = atoi(argv[i] + 3);
-        break;
-      case 't':
-        params->timescalestep = atoi(argv[i] + 3);
-        break;
-      case 's':
-        params->scope = argv[i] + 3;
-        break;
-      default:
-        fprintf(stderr, "unknow param '%c'", argv[i][1]);
-    }
+/* convert a base-94 chan id (!...~) to it integer equivalent (0...93) */
+int chanId(char* str_id) {
+  int id = 0;
+  for (int i = strlen(str_id) - 1; i >= 0; i--) {
+    id = (id * 94) + str_id[i] - '!';
   }
-}
-
-int char2id(char* str_id) {
-  int i = strlen(str_id) - 1, id = 0;
-  for (; i >= 0; i--) {
-    id *= 94;               // shift previous value
-    id += str_id[i] - '!';  //! is 0, ~ is 93
-  }
+  if (id > MAX_CHANNEL) die("%s is over MAX_CHANNEL", str_id);
   return id;
 }
 
-void parseInst(Parameters* params, Parser* p) {
-  char token[32];
-  fscanf(params->fin, "%31s", token);
-  // printf("%s\n",token);
+int unilen(char* s) {
+  int j = 0;
+  for (; *s; s++) j += ((*s & 0xc0) != 0x80);
+  return j;
+}
+
+/* read a $instruction and it opt if needed until $end*/
+void parseVcdInstruction(ParseCtx* p) {
+  char token[MAX_NAME];
+  scanf("%" TXT(SFL) "s", token);
   if (!strcmp("var", token)) {
-    char id_str[4];
-    Channel chan = {};
-    fscanf(params->fin, " reg %d %3[^ ] %" TXT(MAX_NAME) "[^$]", &(chan.size),
-           id_str, chan.name);
-    fscanf(params->fin, " wire %d %3[^ ] %" TXT(MAX_NAME) "[^$]", &(chan.size),
-           id_str, chan.name);
-    int id = char2id(id_str);
-    p->ch[id] = chan;  // printf("size=%i <%c> name=<%s>\n",size,id,data);
-    p->ch[id].scope = p->cur_scopes;
+    char id[MAX_NAME];
+    Channel c = {.scope = p->scope_cur};  // placeholder until id found
+    scanf(" %*s %d %" TXT(SFL) "[^ ] %" TXT(SFL) "[^$]", &c.size, id, c.name);
+    p->ch_lim = MAX(p->ch_lim, strlen(c.name));
+    p->sz_lim = MAX(p->sz_lim, c.size);
+    p->ch[chanId(id)] = c;
   } else if (!strcmp("scope", token)) {
-    fscanf(params->fin, "%*127s %127[^ $]",
-           p->scopes[p->cur_scopes = ++(p->nb_scopes)]);
+    p->scope_count++;
+    if (p->scope_count == MAX_SCOPE) {
+      die("MAX_SCOPE reached\n");
+    }
+    p->scope_cur = p->scope_count;
+    scanf("%*s %" TXT(SFL) "[^ $]", p->scopes[p->scope_cur]);
+    p->scope_lim = MAX(p->scope_lim, strlen(p->scopes[p->scope_cur]));
   } else if (!strcmp("date", token)) {
-    fscanf(params->fin, "\n%31[^$\n]", p->date);
+    scanf("\n%" TXT(SFL) "[^$\n]", p->date);
   } else if (!strcmp("version", token)) {
-    fscanf(params->fin, "\n%31[^$\n]", p->version);
+    scanf("\n%" TXT(SFL) "[^$\n]", p->version);
   } else if (!strcmp("timescale", token)) {
-    fscanf(params->fin, "\n%127[^$\n]", p->scale);
+    scanf("\n%lf%" TXT(SFL) "[^$\n]", &p->scale, p->unit);
   } else if (!strcmp("comment", token)) {
-    fscanf(params->fin, "\n%*[^$]");
+    scanf("\n%*[^$]");
   } else if (!strcmp("upscope", token)) {
-    fscanf(params->fin, "\n%*[^$]");
-    p->cur_scopes = 0;
-  } /*back to root */
-  else if (!strcmp("enddefinitions", token)) {
-    fscanf(params->fin, "\n%*[^$]");
+    scanf("\n%*[^$]");
+    p->scope_cur = 0;  // back to the root
+  } else if (!strcmp("enddefinitions", token)) {
+    scanf("\n%*[^$]");
   } else if (!strcmp("dumpvars", token)) {
   } else if (!strcmp("end", token)) {
   } else {
     printf("unknow token : %s\n", token);
   }
 }
-
-void parseTime(Parameters* params, Parser* p) {
-  long long unsigned stamp = 0;
-  int i, c;
-  while ((c = fgetc(params->fin)) != EOF) {
-    if (isdigit(c)) {
-      stamp = stamp * 10 + (c - '0');
-    } else {
-      ungetc(c, params->fin);
-      if (p->nb >= COUNT(p->timestamps)) return;
-      if (p->nb > 0) {  // copy all previous channels_size val to the current
-                        // one
-        for (i = 0; i < COUNT(p->ch); i++) {
-          p->ch[i].val[p->nb] = p->ch[i].val[p->nb - 1];
-          p->ch[i].type[p->nb] = p->ch[i].type[p->nb - 1];
-        }
-      }
-      p->timestamps[p->nb] = stamp;
-      p->nb++;
-      return;
+/* Parse a time line (ex: '#210000000') and copy all previous samples values */
+void parseVcdTimestamp(ParseCtx* p) {
+  if (p->total >= COUNT(p->ch[0].samples)) {
+    die("MAX_SAMPLE reached");
+  }
+  // copy previous sample on every channel
+  if (p->total > 0) {
+    for (Channel* ch = p->ch; ch < p->ch + COUNT(p->ch); ch++) {
+      ch->samples[p->total] = ch->samples[p->total - 1];
     }
   }
+  scanf("%llu", &p->timestamps[p->total]);
+  p->total++;
 }
 /*
-there is 2 kinds of data definition line :
-state    : 1^
-bus data : b0100001001011001110 ^
+sample line end with the channel ID and start either with a state or data:
+state example: "1^" , "Z^"
+data  example: "b0100 ^"
 */
-void parseData(Parameters* params, Parser* p) {
-  unsigned data = 0;
-  char id_str[4], type = '\0';
-  int id = 0, c = fgetc(params->fin);
-  if (c == 'b') {  // parsing bus data b%[0-9UZX]+ %c
-    while ((c = fgetc(params->fin)) != EOF && c != ' ') {
-      if (isdigit(c))
-        data = data * 2 + (c - '0');
-      else {
-        // letter (Z,U,X,...) = undefined type, but we don't know the id yet
-        if (c == 'x' || c == 'z')
-          type = toupper(c);
-        else
-          type = c;
+void parseVcdSample(ParseCtx* p, int c) {
+  Sample s = {'\0', 0};
+  if (c == 'b') {
+    for (c = getchar(); c != EOF && c != ' '; c = getchar()) {
+      if (c == '0' || c == '1') {
+        s.val = s.val * 2 + (c - '0');
+      } else if (strchr(VALUES, c)) {
+        s.state = c;
+      } else {
+        die("Unknown sample value: %c", c);
       }
     }
-    fscanf(params->fin, "%3[^\n]", id_str);
-    id = char2id(id_str);
-    p->ch[id].type[p->nb - 1] = type;
-    p->ch[id].val[p->nb - 1] = data;
-  } else {  // parsing state %[0-9UZX] %c
-    fscanf(params->fin, "%3[^\n]", id_str);
-    id = char2id(id_str);
-    if (isalpha(c))
-      p->ch[id].type[p->nb - 1] = c;
-    else
-      p->ch[id].type[p->nb - 1] = '\0';  // letter (Z,U,X,...) = undefined type
-    if (isdigit(c)) p->ch[id].val[p->nb - 1] = c - '0';
+  } else {
+    s.state = isalpha(c) ? c : '\0';
+    s.val = isdigit(c) ? c - '0' : 0;
   }
+  char id_str[MAX_NAME];
+  scanf("%" TXT(SFL) "[^\n]", id_str);
+  p->ch[chanId(id_str)].samples[p->total - 1] = s;
 }
 
-void parseFile(Parameters* params, Parser* p) {
-  int c;
-  while ((c = fgetc(params->fin)) != EOF) {
-    //		printf("?%c\n",c);
+void parseVcd(ParseCtx* p) {
+  for (int c = getchar(); c != EOF; c = getchar()) {
     if (isspace(c)) continue;
     if (c == '$') {
-      parseInst(params, p);
-    } else if (isdigit(c) || c == 'b' || c == 'Z' || c == 'U' || c == 'x' ||
-               c == 'z') {
-      if (c == 'x' || c == 'z')
-        ungetc(toupper(c), params->fin);
-      else
-        ungetc(c, params->fin);
-      parseData(params, p);
+      parseVcdInstruction(p);
     } else if (c == '#') {
-      parseTime(params, p);
+      parseVcdTimestamp(p);
+    } else if (strchr(VALUES, c)) {
+      parseVcdSample(p, c);
     } else {
-      fprintf(stderr, "unknow char : %c\n", c);
+      die("unknow char : %c\n", c);
     }
   }
 }
 
-unsigned numDischarges(unsigned n) {
-  unsigned i = 0;
-  do {
-    n = n / 10;
-    i += 1;
-  } while (n > 0);
-  return i;
-}
-void numDelete(char* instr, char* outstr) {
-  unsigned index = 0;
-  for (unsigned i = 0; i < strlen(instr); i++)
-    if (instr[i] < '0' || instr[i] > '9') {
-      outstr[index] = instr[i];
-      ++index;
+void printYml(ParseCtx* p, PrintOpt* opt) {
+  if (unilen(opt->high) != 1) die("high waveform length must be 1");
+  if (unilen(opt->low) != 1) die("low waveform length must be 1");
+  if (unilen(opt->drown) > 1) die("drown waveform length must be 1 or empty");
+  if (unilen(opt->raise) > 1) die("raise waveform length must be 1 or empty");
+
+  int zoom = (p->sz_lim + 7) >> 2;  // how many char per sample (8bit => 2)
+  int trans = *opt->drown && *opt->raise;
+  printf("global:\n");
+  printf("  zoom: %i\n", zoom);
+  printf("  date: %s\n", p->date);
+  printf("  total: %i\n", p->total);
+  printf("  skip: %i\n", opt->skip);
+  printf("  time:\n");
+  printf("    scale: %.2f\n", p->scale);
+  printf("    unit: %s\n", p->unit ?: "?");
+  printf("    %-*s: %s", p->ch_lim, "line", opt->start);
+  for (double smpl = 0; smpl < p->total; smpl += ITV_TIME) {
+    printf("%-*g ", ITV_TIME * zoom - 1, smpl * p->scale);
+  }
+  printf("%s\nchannels:\n", opt->end);
+  for (Channel* ch = p->ch; ch - p->ch < COUNT(p->ch); ch++) {
+    // skip empty ch
+    if (!ch->size) continue;
+    // print scope (if changed)
+    if (ch == p->ch || ch->scope != ((ch - 1)->scope)) {
+      printf("  %s:\n", ch->scope ? p->scopes[ch->scope] : "default");
     }
-  outstr[index] = '\0';
-}
 
-void showVertical(Parameters* params, Parser* p) {
-  int w;
-  unsigned chan, smpl;
-  if (p->nb) fprintf(params->fout, "%i samples", p->nb);
-  if (p->date[0]) fprintf(params->fout, " / %s", p->date);
-  if (p->scale[0]) fprintf(params->fout, " / %s", p->scale);
-  fprintf(params->fout, "\n");
-
-  char scalestr[32] = {'\0'};
-  numDelete(p->scale, scalestr);
-
-  for (chan = 0; chan < COUNT(p->ch); chan++) {
-    if (!p->ch[chan].size) continue;  // skip empty ch
-    if (params->scope && (!p->ch[chan].scope ||
-                          !strstr(params->scope, p->scopes[p->ch[chan].scope])))
-      continue;  // skip root node or unrelated node if scope-only wanted
-    if ((!chan && p->ch[chan].scope) ||
-        (chan > 0 && (p->ch[chan].scope != p->ch[chan - 1].scope))) {
-      unsigned timescalestep = params->timescalestep;
-      if (timescalestep != 0) {
-        fprintf(
-            params->fout, "┌── %s%*.*stime: ",
-            p->ch[chan].scope ? p->scopes[p->ch[chan].scope] : "",
-            params->colsize -
-                strlen(p->ch[chan].scope ? p->scopes[p->ch[chan].scope] : "") -
-                2);
-        for (smpl = 0; smpl < p->nb; smpl += timescalestep) {
-          unsigned scalenum = smpl * atoi(p->scale);
-          fprintf(params->fout, "▏%d%s%*.*s", scalenum, scalestr,
-                  timescalestep * params->width - 1 - numDischarges(scalenum) -
-                      strlen(scalestr));
-        }
-        fprintf(params->fout, "\n│\n");
-      } else
-        fprintf(params->fout, "┌── %s\n",
-                p->ch[chan].scope ? p->scopes[p->ch[chan].scope] : "");
-    }
-    fprintf(params->fout, "%s %*.*s[%2i]: ", p->ch[chan].scope ? "│" : " ",
-            params->colsize, params->colsize, p->ch[chan].name,
-            p->ch[chan].size);
-    for (smpl = 0; smpl < p->nb; smpl += 1) {
-      char type = p->ch[chan].type[smpl];
-      unsigned data = p->ch[chan].val[smpl];
-      if (p->ch[chan].size == 1) {  // binary
-        w = params->width;
-        // have a previous data => can print a transition
-        if (params->round && smpl > 0 && !p->ch[chan].type[smpl - 1]) {
-          if (p->ch[chan].val[smpl] !=
-              p->ch[chan].val[smpl - 1]) {  // the value changed
-            // from H to L or L to H ?
-            fprintf(params->fout, params->round == 2 ? "%s" : "│",
-                    (p->ch[chan].val[smpl - 1] ? "╲" : "╱"));
-            w -= 1;
-          }
-        }
-        while (w-- > 0) {
-          if (type)
-            fprintf(params->fout, "%c", type);
+    printf("    %-*s: %s", p->ch_lim, ch->name, opt->start);
+    for (Sample* s = ch->samples + opt->skip; s < ch->samples + p->total; s++) {
+      Sample* prev = s > ch->samples ? s - 1 : s;
+      if (s->state) {  // state data: UUUUZZZZ-
+        printf("%-*c", zoom, s->state);
+      } else if (ch->size == 1) {  // binary wave: ▁▁/▔▔
+        // have a different data => print a transition
+        for (int w = 0; w < zoom; w++) {
+          if (!w && trans && s->state == prev->state && s->val != prev->val)
+            printf("%s", prev->val ? opt->drown : opt->raise);
           else
-            fprintf(params->fout, "%s", data ? "▔" : "▁");
+            printf("%s", s->val ? opt->high : opt->low);
         }
-      } else {  // bus
-        int is_bin = p->ch[chan].type[smpl];
-        fprintf(params->fout, is_bin ? "%*c" : "%*X", params->width,
-                is_bin ? p->ch[chan].type[smpl] : p->ch[chan].val[smpl]);
+      } else {  // bus : show hex value
+        printf("%-*X", zoom, s->val);
       }
     }
-    fprintf(params->fout, "%s%s\n", params->width >= 2 ? "\n" : " ",
-            p->ch[chan].scope && params->width >= 2 ? "│" : " ");
+    printf("%s\n", opt->end);
   }
 }
 
 int main(int argc, char** argv) {
-  Parameters params = {
-      .verbose = 0,
-      .width = 2,
-      .round = 2,
-      .colsize = 32,
-      .timescalestep = 0,
-      .fin = stdin,
-      .fout = stdout,
-  };
-  Parser data;
-  memset(&data, 0, sizeof(Parser));
-
-  parseArgs(argc, argv, &params);
-  if (!params.fin) {
-    return fprintf(stderr, "no input stream\n"), -1;
-  }
-  parseFile(&params, &data);
-  showVertical(&params, &data);
-  fclose(params.fin);
+  PrintOpt opt = {getenv("LOW") ?: "▁",       getenv("RAISE") ?: "╱",
+                  getenv("HIGH") ?: "▔",      getenv("DROWN") ?: "╲",
+                  getenv("STX") ?: "\"",      getenv("ETX") ?: "\"",
+                  atoi(getenv("SKIP") ?: "0")};
+  // PrintOpt opt = {"_", "/", "#", "\\"} {"▁", "╱", "▔", "╲"};
+  ParseCtx ctx = {};
+  parseVcd(&ctx);
+  printYml(&ctx, &opt);
   return 0;
 }
